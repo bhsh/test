742e31f71e5b2ec1dfdbe19e3ea688e5518fe6a1
unknown <tz68d9@DLF4X2NX1.Asia.DelphiAuto.net>
2015-03-05 19:59:48 +0800

    missing

--- Debug/.sync_on_halt.o.d
+++
@@ -1,6 +0,0 @@
-sync_on_halt.o :	..\sync_on_halt.c
-..\sync_on_halt.c :	
-sync_on_halt.o :	"D:\InfineonToolsSetup\Tasking3.5.1\ctc\include\stddef.h"
-"D:\InfineonToolsSetup\Tasking3.5.1\ctc\include\stddef.h" :	
-sync_on_halt.o :	"D:\InfineonToolsSetup\Tasking3.5.1\ctc\include\sfr\regtc1782.sfr"
-"D:\InfineonToolsSetup\Tasking3.5.1\ctc\include\sfr\regtc1782.sfr" :	
